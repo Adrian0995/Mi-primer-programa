d46e5ed37b8495fa6cffaf8d832b5782e80861e0
Adrian0995 <adrianlv0995@gmailc.om>
2018-12-31 21:56:21 -0500

    Añadiendo el archivo convertidor_grados

---
+++ Convertidor_grados.py
@@ -0,0 +1,13 @@
+fahrenheit = int(input("¿Cuatos grados Fahrenheit quieres convertir a Celsius? : "))
+
+celsius = (fahrenheit - 32) * 5/9
+
+print("{} grados Fahrenheit son: {} Celsius".format(fahrenheit , celsius))
+
+#Fahrenheit = int(input("Enter a temperature in Fahrenheit: "))
+
+
+#Celsius = (Fahrenheit - 32) * 5.0/9.0
+
+#print ("Temperature:", Fahrenheit, "Fahrenheit = ", Celsius, " C")
+
