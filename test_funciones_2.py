af8748f20644412faafce649d6ae68887b780cac
adrianlv95 <adrianlv0995@gmail.com>
2019-01-12 18:45:08 -0500

    añadiendo archivo test funciones 1

---
+++ test_funciones_2.py
@@ -0,0 +1,16 @@
+
+def reverse_string(string_to_reverse):
+    string_reversed = ""
+    current_index = len(string_to_reverse)-1
+    while current_index >= 0:
+        string_reversed += string_to_reverse[current_index]
+        current_index -= 1
+    return string_reversed
+
+
+print(reverse_string("Hola"))
+print(reverse_string("Chau"))
+print(reverse_string("Adrian"))
+print(reverse_string("Kafu"))
+print(reverse_string("Chela"))
+print(reverse_string("Ivan"))
